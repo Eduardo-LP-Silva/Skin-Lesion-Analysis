0369bc81bcc7009da8c3815785e3b6342dd8c04c
Joana Ramos <joanasmramos98@gmail.com>
2020-05-23 18:29:36 +0100

    Structure code

--- src/task1.py
+++ src/task1.py
@@ -34,16 +34,28 @@ def process_path(file_path):
 def load_dataset(data_dir):
     list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*.jpg'))
     labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-    for image, label in labeled_ds.take(1):
-        print("Image shape: ", image.numpy().shape)
-        print("Label: ", label.numpy())
+    # for image, label in labeled_ds.take(1):
+    #     print("Image shape: ", image.numpy().shape)
+    #     print("Label: ", label.numpy())
+    return labeled_ds
+
+def train_VGG(dataset):
+    input_tensor = tf.keras.Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3), batch_size=30, dtype=tf.float32) # batch_size=30
+    model = tf.keras.applications.VGG16(include_top=False, weights='imagenet', 
+    input_tensor=input_tensor, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3),
+    pooling='max', classes=2, classifier_activation='softmax')
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+    history = model.fit(dataset, batch_size=30, epochs=2, verbose=1) #
+
 
 def main() :
     data_dir = pathlib.Path(DIRECTORY)
     image_count = len(list(data_dir.glob('*/*.jpg')))
     global CLASS_NAMES 
     CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
-    load_dataset(data_dir)
+    dataset = load_dataset(data_dir)
+    # train_VGG(dataset)
+
 
 if __name__ == "__main__":
     main()
