a02104b658f7409a3379c19614fa3302c481fa9b
João Agulha <jotaagulha@hotmail.com>
2020-06-08 22:27:59 +0100

    Change image files

--- src/bag_of_words/feature-detection.py
+++ src/bag_of_words/feature-detection.py
@@ -26,26 +26,33 @@ BOW = 'bow'
 TRAIN = 'train'
 TEST = 'test'
 TESTONE = 'testone'
-#-----------------------------------------------------------------------------------------------------------
 
-IMG_PATH = 'images'
-ANNOT_PATH = 'annotations'
-DESC_PATH = 'descriptors.pkl'
+#-----------------------------------------------------------------------------------------------------------
+IMG_PATH_TRAIN = 'images/train'
+IMG_PATH_TEST = 'images/test'
+
+
+ANNOT_PATH_TRAIN = 'annotations/train'
+ANNOT_PATH_TEST = 'annotations/test'
+
+DESC_PATH_TRAIN = 'descriptors_train.pkl'
+
 BOW_PATH = 'bow.pkl'
 SVM_TRAIN_PATH = 'model.pkl'
 VARS_TRAIN_PATH = 'train.pkl'
 
-db = Database(IMG_PATH, ANNOT_PATH)
+db_train = Database(IMG_PATH_TRAIN, ANNOT_PATH_TRAIN)
+db_test = Database(IMG_PATH_TEST, ANNOT_PATH_TEST)
 
 
 labels_train = {
     'lower_bound': 0,
-    'upper_bound': 30, #900
+    'upper_bound': 11402, #11402
 }
 
 labels_test = {
-    'lower_bound': 40,
-    'upper_bound': 60, #900
+    'lower_bound': 0,
+    'upper_bound': 11392, #11392
 }
 
 keys = {
@@ -54,17 +61,22 @@ keys = {
 }
 
 def main():
-    global db
+    global db_train
+    global db_test
 
     with open("ISBI2016_ISIC_Part3_Training_GroundTruth.csv") as f:
         keys = {line.split(',')[0]: line.split(',')[1].partition("\n")[0] for line in f}
 
-    descriptors = []
-    features = []
+    with open("TEGT.csv") as f:
+        keys.update({line.split(',')[0]: line.split(',')[1].partition("\n")[0] for line in f})
+
+    descriptors_train = []
+    features_train = []
+    features_test = []
     if not DESC:
         try:
             print('Loading descriptors file from path: ', BOW_PATH)
-            descriptors = load_object(DESC_PATH)
+            descriptors_train = load_object(DESC_PATH_TRAIN)
             print('Finished loading descriptors')
         except Exception:
             print('Unable to load descriptors file')
@@ -76,48 +88,45 @@ def main():
     while file_it < labels_train['upper_bound'] :
         try:
             label = 'ISIC_'
-            file_it = file_it + 1
             name = label + str(file_it).zfill(7)
-            img = db.read_img(name)
-            print('Computing descriptors for ' + str(label) + str(file_it).zfill(7) + ' of ' + str(labels_train['upper_bound']))
+            img = db_train.read_img(name)
             img = gray(img)
             img = resize_img(img)
             feature = get_key_points(img)
-            features.append((name, img, feature))
+            features_train.append((name, img, feature))
             if DESC:
-                descriptors.extend(feature[1])
+                descriptors_train.extend(feature[1])
+            print('Computing descriptors for ' + str(label) + str(file_it).zfill(7))
             i = i + 1
         except:
             continue
-    labels_train['upper_bound'] = i
+        finally: 
+            file_it = file_it + 1
 
     file_it = labels_test['lower_bound']
     while file_it < labels_test['upper_bound'] :
         try:
             label = 'ISIC_'
-            file_it = file_it + 1
             name = label + str(file_it).zfill(7)
-            img = db.read_img(name)
-            print('Computing descriptors for ' + str(label) + str(file_it).zfill(7) + ' of ' + str(labels_test['upper_bound']))
+            img = db_test.read_img(name)
             img = gray(img)
             img = resize_img(img)
             feature = get_key_points(img)
-            features.append((name, img, feature))
-            if DESC:
-                descriptors.extend(feature[1])
             i = i + 1
+            features_test.append((name, img, feature))
+            print('Computing descriptors for ' + str(label) + str(file_it).zfill(7))
         except:
             continue
-    labels_test['lower_bound'] = labels_train['upper_bound']
-    labels_test['upper_bound'] = i
+        finally: 
+            file_it = file_it + 1
 
 
     print('Finished computing descriptors')
     print('Storing descriptors')
-    store_object(descriptors, DESC_PATH)
+    store_object(descriptors_train, DESC_PATH_TRAIN)
     print('Finished storing descriptors')
     
-    descriptors = array_to_np(descriptors)
+    descriptors_train = array_to_np(descriptors_train)
     dictionary = None
 
     if not BOW:
@@ -131,19 +140,31 @@ def main():
     else:
         print('Computing bag of words')
         bow = bow_trainer(100)
-        dictionary = bow_cluster(bow, descriptors)
+        dictionary = bow_cluster(bow, descriptors_train)
         print('Finished computing bag of words')
         print('Storing bag of words')
         store_object(dictionary, BOW_PATH)
         print('Finished storing bag of words')
 
-    X = []
-    y = []
     print('Computing target variables')
+    X_train = []
+    y_train = []
+    X_test = []
+    y_test = []
     extractor = bow_extractor(dictionary)
-    for name, img, feature in features:
-            X.append(bow_extract(extractor, img, feature[0])[0])
-            y.append(1 if keys[name] == 'malignant' else 0)
+    for name, img, feature in features_train:
+        try:
+            X_train.append(bow_extract(extractor, img, feature[0])[0])
+            y_train.append(1 if keys[name] == 'malignant' or keys[name] == '0.0' else 0)
+        except:
+            print('aqui')
+
+    for name, img, feature in features_test:
+        try:
+            X_test.append(bow_extract(extractor, img, feature[0])[0])
+            y_test.append(1 if keys[name] == 'malignant' or keys[name] == '0.0' else 0)
+        except:
+            print('aqui')
     print('Finish computing target variables')
 
     svm = None
@@ -158,21 +179,17 @@ def main():
     else:
         print('Training model')
         svm = create_svm()
-        X_train = X[labels_train['lower_bound']:labels_train['upper_bound']]
-        y_train = y[labels_train['lower_bound']:labels_train['upper_bound']]
         train_svm(svm, X_train, y_train)
         print('Finished training model')
 
         print('Saving model')
-        store_object([X,y], VARS_TRAIN_PATH)
+        store_object([X_train,y_train], VARS_TRAIN_PATH)
         store_svm(svm, SVM_TRAIN_PATH)
         print('Finished saving model')
 
 
     if TEST:
         print('Testing model')
-        X_test = X[labels_test['lower_bound']:labels_test['upper_bound']]
-        y_test = y[labels_test['lower_bound']:labels_test['upper_bound']]
         pred = np.squeeze(test_svm(svm, X_test)[1].astype(int))
         print('Finished testing model')
 
@@ -188,10 +205,10 @@ def main():
         recall = cm[0][0]/(cm[0][0] + cm[0][1])*100
         f1 = 2 * precision * recall / (precision + recall)
 
-        print('Accuracy : ', accuracy, '%')
-        print('Precision: ', precision, '%')
-        print('Recall   : ', recall, '%')
-        print('F1       : ', f1)
+        print('Accuracy : ', round(accuracy, 2), '%')
+        print('Precision: ', round(precision, 2), '%')
+        print('Recall   : ', round(recall, 2), '%')
+        print('F1       : ', round(f1, 2))
 
 if __name__ == "__main__":
     main()
